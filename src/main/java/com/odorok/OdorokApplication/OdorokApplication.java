5b11004dc8bc0162f5ef7cdc8895140716957d0a
Lee Wonjun <evenil0206@gmail.com>
2025-06-02 18:06:58 +0900

    최초 커밋

---
+++ src/main/java/com/odorok/OdorokApplication/OdorokApplication.java
@@ -0,0 +1,13 @@
+package com.odorok.OdorokApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdorokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdorokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odorok/OdorokApplication/OdorokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odorok.OdorokApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdorokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

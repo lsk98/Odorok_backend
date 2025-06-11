25df3241ef90a238f4b0b7a36e2e4e159f832dd6
ParkShinyeong <syngh503@gmail.com>
2025-06-11 21:00:15 +0900

    feat(security): jwt 토큰 생성 클래스
    accessToken, refreshToken 생성 메소드,
    claim 추출 메소드

--- src/main/java/com/odorok/OdorokApplication/security/jwt/JwtUtil.java
+++ src/main/java/com/odorok/OdorokApplication/security/jwt/JwtUtil.java
@@ -1,2 +1,51 @@
-package com.odorok.OdorokApplication.security.jwt;public class JwtUtil {
+package com.odorok.OdorokApplication.security.jwt;
+
+import com.odorok.OdorokApplication.security.dto.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+@Slf4j
+public class JwtUtil {
+    private final SecretKey key;
+
+    public JwtUtil() {
+        key = Jwts.SIG.HS256.key().build();
+    }
+
+    @Value("${jwt.access.expmin}")
+    private long accessExpMin;
+    @Value("${jwt.refresh.expmin}")
+    private long refreshExpMin;
+
+    public String createAccessToken(User user) {
+        return create("accessToken", accessExpMin,
+                Map.of( "email", user.getEmail(), "nickname", user.getNickname(), "name", user.getName()))
+    }
+
+    public String createRefreshToken(User user) {
+        return create("refreshToken", refreshExpMin,
+                Map.of("email", user.getEmail()));
+    }
+
+    public String create(String subject, long expireMin, Map<String, Object> claims) {
+        Date expireDate = new Date(System.currentTimeMillis() + 1000 * 60 * expireMin);
+        String token = Jwts.builder().subject(subject).claims(claims).expiration(expireDate).signWith(key).compact();
+        log.debug("token 생성: {}", token);
+        return token;
+    }
+
+    public Claims getClaims(String jwt) {
+        var parser = Jwts.parser().verifyWith(key).build();
+        var jws = parser.parseSignedClaims(jwt);
+        log.debug("claims: {}", jws.getPayload());
+        return jws.getPayload();
+    }
 }

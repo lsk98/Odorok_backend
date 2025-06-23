aeb885751f4b172f8f8efa65e8fa88d0acc2506c
Lee Wonjun <evenil0206@gmail.com>
2025-06-23 17:41:56 +0900

    프롬프트로 이름 수정

--- src/main/java/com/odorok/OdorokApplication/gpt/GptService.java
+++ src/main/java/com/odorok/OdorokApplication/gpt/GptService.java
@@ -1,5 +1,6 @@
 package com.odorok.OdorokApplication.gpt;
 
+import com.nimbusds.openid.connect.sdk.Prompt;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,15 +9,15 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 public interface GptService {
-    String sendMessage(String context, Message newMessage);
+    String sendPrompt(String context, Prompt prompt);
 
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
-    class Message {
+    class Prompt { // 프롬프트 생성을 위한 인터페이스 : role 은 system(설정용 프롬프트), user(사용자 프롬프트), agent(gpt)
         String role;
-        String message;
+        String prompt;
         LocalDateTime date;
     }
 }

5deea27305182656e9562a98260132df8438c4f2
Lee Wonjun <evenil0206@gmail.com>
2025-07-03 03:35:50 +0900

    temp commit

--- src/main/java/com/odorok/OdorokApplication/diary/repository/VisitedCourseRepository.java
+++ src/main/java/com/odorok/OdorokApplication/diary/repository/VisitedCourseRepository.java
@@ -1,7 +1,10 @@
-package com.odorok.OdorokApplication.diary.repository;
+package com.odorok.OdorokApplication.course.repository;
 
 import com.odorok.OdorokApplication.domain.VisitedCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface VisitedCourseRepository extends JpaRepository<VisitedCourse, Long>, VisitedCourseRepositoryCustom  {
+@Repository
+public interface VisitedCourseRepository extends JpaRepository<VisitedCourse, Long>, VisitedCourseRepositoryCustom {
+    boolean existsByUserIdAndCourseId(Long userId, Long courseId);
 }

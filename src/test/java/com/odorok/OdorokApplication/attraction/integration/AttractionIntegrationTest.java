c09d095edb7b401243e354e900a773b133bd214d
Lee Wonjun <evenil0206@gmail.com>
2025-07-05 11:24:21 +0900

    region integration test

--- src/test/java/com/odorok/OdorokApplication/attraction/integration/AttractionIntegrationTest.java
+++ src/test/java/com/odorok/OdorokApplication/attraction/integration/AttractionIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.odorok.OdorokApplication.attraction.integration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -8,4 +10,6 @@ public class AttractionIntegrationTest {
     @LocalServerPort
     private Integer LOCAL_PORT;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
 }

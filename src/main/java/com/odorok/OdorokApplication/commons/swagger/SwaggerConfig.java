6b649e373b0e93cc7b241888db8fd5aeb9e91997
parkshinyeong <syngh503@gmail.com>
2025-07-30 00:00:14 +0900

    feat: Swagger Jwt Authentication 추가
    swagger ui에서 jwt token 사용을 위한 설정 추가

--- src/main/java/com/odorok/OdorokApplication/commons/swagger/SwaggerConfig.java
+++ src/main/java/com/odorok/OdorokApplication/commons/swagger/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.odorok.OdorokApplication.commons.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +15,20 @@ public class SwaggerConfig {
     private static final String DESCRIPTION = "한국 관광공사 데이터 활용 공모전.";
     @Bean
     public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         Info info = new Info().title(TITLE).version(VERSION).description(DESCRIPTION);
-        return new OpenAPI().components(new Components()).info(info);
+        return new OpenAPI().info(info)
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)) // API 전체에 보안 적용
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("JWT 기반 인증을 위한 토큰을 입력하세요.<br>" +
+                                        "예: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
+                        )
+                );
     }
 }
